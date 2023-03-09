b02bee6bd290349ac2a32035c21fedff9da091d5
Jonathan Spira <jjspira@gmail.com>
2023-03-09 01:08:50 -0500

    progress on test cases, getting ready to write the VM

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,17 @@
+mod proto;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use prost::{bytes::Buf, Message};
+
+    const TEST_PROGRAM: &[u8] = include_bytes!("../proto/test.yarnc");
+
+    #[test]
+    fn test() {
+        let program = proto::Program::decode(TEST_PROGRAM).unwrap();
+        println!("{:#?}", program);
+
+        panic!()
+    }
+}
